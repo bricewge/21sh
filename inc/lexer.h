c96af4491c4aa725848bf7b1a3521a4ce6b90fcc
Brice Waegeneire <brice.wge@gmail.com>
2017-07-04 18:02:50 +0200

    initial commit

---
+++ inc/lexer.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/27 19:24:35 by bwaegene          #+#    #+#             */
+/*   Updated: 2017/01/27 20:08:00 by bwaegene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+# include "libft.h"
+
+enum				e_categories
+{
+	WORD, ASSIGNMENT_WORD, NAME, NEWLINE, IO_NUMBER,
+	ANDIF, ORIF, DSEMI,
+	DLESS, DGREAT, LESSAND, GREATAND, LESSGREAT, DLESSDASH,
+	CLOBBER,
+	If, Then, Else, Elif, Fi, Do, Done,
+	Case, Esac, While, Until, For,
+	Lbrace, Rbrace, Bang,
+	In
+};
+
+typedef struct		s_token
+{
+	char 			*str;
+	int				cat;
+}
+
+#endif
